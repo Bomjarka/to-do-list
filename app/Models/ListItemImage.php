b5eabc67d4ede944efea4375ccf4a70ea5ece7c6
a.chirkin <a.chirkin@robo.finance>
2023-07-31 14:24:04 +0700

    add properties

--- app/Models/ListItemImage.php
+++ app/Models/ListItemImage.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $preview_name
+ * @property int $list_item_id
+ */
 class ListItemImage extends Model
 {
     use HasFactory;
--- app/Models/ToDoList.php
+++ app/Models/ToDoList.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int id
  * @property int $user_id
  * @property string $name
  */
